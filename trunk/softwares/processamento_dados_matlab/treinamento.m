c117644ae84388b9743c26b36c7cad812b3469c0
diogolr <diogolr@114122ef-0f80-af60-45d5-5317cd78b39a>
2010-08-25 12:46:36 +0000

    Adiçaõ das planilhas das falhas: FAQ, FASR, FAVK e FSiVzT.

--- trunk/softwares/processamento_dados_matlab/treinamento.m
+++ trunk/softwares/processamento_dados_matlab/treinamento.m
@@ -5,7 +5,7 @@ clc;
 close all;
 
 % falha = input( 'Sigla da falha: ' );
-falha = 'FASR';
+falha = 'FSiVzT';
 
 % arq_niveis = input( 'Niveis [treinamento]: ' );
 arq_niveis = strcat( '..\simulacao_tanques_cpp\saidas\', ...
