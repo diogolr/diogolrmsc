2df3413edc3c8e3a19242981640c3a970fdb6177
diogolr <diogolr@114122ef-0f80-af60-45d5-5317cd78b39a>
2010-10-05 19:53:20 +0000

    Finalização do capítulo de detecção e diagnóstico de falhas da qualificação (versão não revisada) e modificação dos arquivos de matlab para gerar os resultados da qualificação.
    Olhar o que foi pensado para ser feito no arquivo 'resultados_qualificacao.m'.

---
+++ trunk/softwares/processamento_dados_matlab/resultados_qualificacao.m
@@ -0,0 +1,80 @@
+clear;
+clc;
+close all;
+
+% falha = input( 'Sigla da falha: ' );
+falhas = { 'FSeDG' };% 'FSeDO' 'FSeSR' 'FSeQ' 'FADG' 'FADO' 'FASR' 'FAVK' ...
+%            'FAQ' 'FSiVzT' 'FSiVrOS' 'FSiVrGMP' 'FSiEOS' };
+
+% Melhores redes para as deteccoes de falha
+% Cada linha contem [ORDEM NCO TREINAMENTO]
+melhores = [ 4 28 2 ];%; ... % FSeDG
+%              4 28 5 ; ... % FSeDO
+%              4 20 3 ; ... % FSeSR
+%              4 20 4 ; ... % FSeQ
+%              2  8 3 ; ... % FADG
+%              4 28 3 ; ... % FADO
+%              2  8 6 ; ... % FASR
+%              2  8 5 ; ... % FAVK
+%              4 28 6 ; ... % FAQ
+%              4 24 1 ; ... % FSiVzT
+%              2  8 3 ; ... % FSiVrOS
+%              4 20 2 ; ... % FSiVrGMP
+%              2 12 4 ; ... % FSiEOS
+%            ];
+
+for i = 1 : length( falhas )
+    disp( falhas{i} );
+    
+    falha = falhas{i};
+    
+    % pasta_valid = input( 'Pasta dos arquivos de validacao: ' );
+    pasta_saidas = strcat( '../simulacao_tanques_cpp/saidas/', falha, '/' );
+
+    % pasta_rnas = input( 'Pasta dos arquivos das redes treinadas: ' );
+    pasta_rnas = strcat( '../../dados/deteccao/', falha, '/' );
+    
+    % Determinando o nome do arquivo de configuracao da melhor rede
+    nome_arq_sem_ext = strcat( pasta_rnas, ...
+                               'O', num2str( melhores( i, 1 ) ), '/', ...
+                               'N', num2str( melhores( i, 2 ) ), '/', ...
+                               'O', num2str( melhores( i, 1 ) ), ...
+                               'N', num2str( melhores( i, 2 ) ), ...
+                               'T', num2str( melhores( i, 3 ) ) );
+
+    arq_config_detec = strcat( nome_arq_sem_ext, '.mat' );
+
+    load( arq_config_detec );
+    
+    rede_detec = rede;
+    
+    % Numero de regressores = ORDEM - 1
+    ordem = melhores( i, 1 );
+    
+    regressores = ordem - 1;
+        
+    % Lendo o arquivo de validacao ------------------------------------
+    arq_niveis = strcat( pasta_saidas, 'niveis_qualif.dat' );
+    arq_erro_sc = strcat( pasta_saidas, 'erro_sc_qualif.dat' );
+
+    [entrada saida residuos] = config_ent_sai_valid( arq_niveis, ...
+                                                     arq_erro_sc, ...
+                                                     regressores );
+
+    % CONTINUAR AQUI ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    % PENSAR COMO EXIBIR OS RESIDUOS (olhar dissertacao de marcela)
+    %
+    % PENSAR COMO EXIBIR A DETECCAO DA FALHA
+    %
+    % VER SE NAO EH MELHOR GERAR UMA FALHA E VOLTAR PARA O ESTADO NORMAL
+    % PARA GERAR A FALHA SEGUINTE
+
+%     % Determinando o nome do arquivo final e chamando o metodo para 
+%     % gerar a saida
+%     nome_final = strcat( nome_arq_sem_ext, '_qualif' );
+% 
+%     gerar_saida_tikz( rede_detec, entrada, saida, nome_final );
+      
+    % Limpando as variaveis de ambiente carregadas com o load
+    clear rede rede_detec arq_config lim_ent lim_sai tempo_treinamento;
+end
